d35af669efbb8a826e5fc050e48834193d39ced6
flammadc <acc.for.m33@gmail.com>
2022-07-29 07:03:11 +0700

    First commit

---
+++ app/Http/Controllers/OutController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Out;
+use Illuminate\Http\Request;
+
+class OutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Out  $out
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Out $out)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Out  $out
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Out $out)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Out  $out
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Out $out)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Out  $out
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Out $out)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_07_28_073519_create_outs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('outs', function (Blueprint $table) {
+            $table->id();
+            $table->string("price");
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->integer("quantity");
+            $table->string("amount");
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('outs');
+    }
+};
